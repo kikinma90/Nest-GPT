62d1db2caaabb934d18e24bedfd41a35a74f0c06
kikinma90 <juan_manuel90@msn.com>
2024-01-02 06:14:57 +0100

    Realizado la parte back en nest del caso de uso de correción de ortografía, realizando la conexión a la API de OpenAI

---
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -0,0 +1,55 @@
+import OpenAI from "openai";
+
+
+interface Options {
+    prompt: string;
+}
+
+export const orthographyCheckUseCase = async(openai: OpenAI, options: Options) => {
+
+    const { prompt } = options;
+
+    const completion = await openai.chat.completions.create({
+        messages: [
+            { 
+                role: "system", 
+                content: `
+                Te serán proveídos textos en español con posibles errores ortográficos y gramaticales,
+                Las palabras usadas deben existir en el diccionario de la Real Academia Española,
+                debes de responder en formato JSON,
+                tu tarea es corregirlos y retornar información de las solicitudes,
+                también debes de dar un porcentaje de acierto por el usuario,
+                
+                Si no hay errores, debes de retornar un mensaje de felicitaciones.
+
+                Ejemplo de salida: 
+                {
+                    userScore: number,
+                    errors: string[], // ['error -> solución']
+                    message: string, // Usa emojis y texto para felicitar al usuario
+                }
+                
+                
+                `
+            },
+            {
+                role: "user",
+                content: prompt,
+            }
+        ],
+        model: "gpt-4",
+        temperature: 0.3,
+        max_tokens: 150,
+        // Esrto no lo soportan todos los modelos
+        // response_format: {
+        //     type: 'json_object'
+        // }
+      });
+    
+    //   console.log(completion);
+
+      const jsonResp = JSON.parse(completion.choices[0].message.content);
+
+      return jsonResp;
+
+}
